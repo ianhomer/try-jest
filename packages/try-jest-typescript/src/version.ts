a27d1c45d23a95780946123b6fde197155d4ee17
Ian Homer <code@ianhomer.com>
2022-09-13 22:56:05 +0100

    ♻️ change to named matching groups

--- packages/try-jest-typescript/src/version.ts
+++ packages/try-jest-typescript/src/version.ts
@@ -1,13 +1,16 @@
+const increment = (part: string) => parseInt(part) + 1;
 export const version = (version: string, commits: number, branch: string) => {
-  const match = version.match(/v([0-9]+)\.([0-9]+)\.([0-9]+)/);
-  if (!match) {
-    throw "Version ${version} is not like vx.y.z";
+  const { major, minor, patch } = version.match(
+    /v(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)/
+  ).groups;
+  if (!major) {
+    throw `Version ${version} is not like vx.y.z`;
   }
   if (commits == 0) {
     return version;
   }
   if (branch.startsWith("release/")) {
-    return `v${match[1]}.${match[2]}.${parseInt(match[3]) + 1}-rc`;
+    return `v${major}.${minor}.${increment(patch)}-rc`;
   }
-  return `v${match[1]}.${parseInt(match[2]) + 1}.0-rc`;
+  return `v${major}.${increment(minor)}.0-rc`;
 };
