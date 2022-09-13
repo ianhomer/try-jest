ca543d1b2dae300461c84f2ec9281e7b0bc34595
Ian Homer <code@ianhomer.com>
2022-09-13 22:47:49 +0100

    ✅ expand on unit tests

--- packages/try-jest-typescript/src/version.test.ts
+++ packages/try-jest-typescript/src/version.test.ts
@@ -1,18 +1,31 @@
 import { version } from "./version";
 
 describe("version", () => {
-  test("should unchange when from tag", () => {
-    expect(version("v1.2.3", 0, "main")).toBe("v1.2.3");
-  });
-
-  test("should increment patch when changed on release branch", () => {
-    expect(version("v1.2.3", 1, "release/1.2")).toBe("v1.2.4-rc");
+  test.each`
+    tag
+    ${"v1.2.0"}
+    ${"v1.2.1"}
+  `("should unchange when from tag", ({ tag }) => {
+    expect(version(tag, 0, "main")).toBe(tag);
   });
 
   test.each`
-    tag         | expected
-    ${"v1.2.0"} | ${"v1.3.0-rc"}
-    ${"v1.2.3"} | ${"v1.3.0-rc"}
+    tag          | expected
+    ${"v1.2.0"}  | ${"v1.2.1-rc"}
+    ${"v1.2.3"}  | ${"v1.2.4-rc"}
+    ${"v2.10.0"} | ${"v2.10.1-rc"}
+  `(
+    "should increment patch when changed on release branch",
+    ({ tag, expected }) => {
+      expect(version(tag, 1, "release/1.2")).toBe(expected);
+    }
+  );
+
+  test.each`
+    tag          | expected
+    ${"v1.2.0"}  | ${"v1.3.0-rc"}
+    ${"v1.2.3"}  | ${"v1.3.0-rc"}
+    ${"v2.10.0"} | ${"v2.11.0-rc"}
   `(
     "should increment minor when changed not on release branch",
     ({ tag, expected }) => {
@@ -24,6 +37,7 @@ describe("version", () => {
     tag
     ${"1.2"}
     ${"v1.2"}
+    ${"v"}
   `("should throw error when version not valid", ({ tag }) => {
     expect(() => version(tag, 0, "foo")).toThrow();
   });
