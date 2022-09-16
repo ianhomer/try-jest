c5e3256e1b6e28907025246181df927c4c9940c7
Ian Homer <code@ianhomer.com>
2022-09-16 13:17:46 +0100

    🚨 remove unnecessary import react

--- packages/try-jest-react/src/Actor.tsx
+++ packages/try-jest-react/src/Actor.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Actor() {
   return (
     <div className="actor">
--- packages/try-jest-react/src/Timer.tsx
+++ packages/try-jest-react/src/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Timer() {
   const [timeoutTime, setTimeoutTime] = useState(0);
