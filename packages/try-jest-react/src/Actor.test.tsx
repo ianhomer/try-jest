cd23aa672d2fee3b5659da9fab438d84ae9c58a8
Ian Homer <code@ianhomer.com>
2022-09-16 13:15:50 +0100

    ✅ add extra jest types

---
+++ packages/try-jest-react/src/Actor.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Actor from "./Actor";
+
+describe("actor", () => {
+  test("renders OK", () => {
+    render(<Actor/>);
+    const textElement = screen.getByText(/actor-name/i);
+    expect(textElement).toBeInTheDocument();
+  });
+});
---
+++ packages/try-jest-react/src/Actor.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Actor() {
+  return (
+    <div className="actor">
+      <h1>actor-name</h1>
+    </div>
+  );
+}
+
+export default Actor
+
