299ab99717626d7ba8615e8d4f5d120f4d51fbd7
Ian Homer <code@ianhomer.com>
2023-06-24 11:13:22 +0100

    ✅ test match object variations

--- packages/try-jest-typescript/src/match-object.test.ts
+++ packages/try-jest-typescript/src/match-object.test.ts
@@ -3,6 +3,9 @@ describe("match object", () => {
     expect({ a: 1 }).toMatchObject({ a: 1 });
   });
   test("deep equality", () => {
-    expect({ a: 1, b: { c: 2 } }).toMatchObject({ a: 1, b: { c: 2 } });
+    expect({ a: 1, b: { c: 3 }, d: 4 }).toMatchObject({ a: 1, b: { c: 3 } });
+  });
+  test("deep equality, swapped", () => {
+    expect({ b: { c: 3 }, a: 1, d: 4 }).toMatchObject({ b: { c: 3 }, a: 1 });
   });
 });
