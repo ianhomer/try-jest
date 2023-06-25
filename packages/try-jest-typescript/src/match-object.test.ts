3dfe79870a092a70d699cce60daf19617d367db6
Ian Homer <code@ianhomer.com>
2023-06-25 08:18:23 +0100

    🧪 display failing reason

--- packages/try-jest-typescript/src/match-object.test.ts
+++ packages/try-jest-typescript/src/match-object.test.ts
@@ -3,9 +3,20 @@ describe("match object", () => {
     expect({ a: 1 }).toMatchObject({ a: 1 });
   });
   test("deep equality", () => {
-    expect({ a: 1, b: { c: 3 }, d: 4 }).toMatchObject({ a: 1, b: { c: 3 } });
+    expect({ a: 1, b: { c: 3 }, d: "important message" }).toMatchObject({
+      a: 1,
+      b: { c: 3 },
+    });
   });
   test("deep equality, swapped", () => {
     expect({ b: { c: 3 }, a: 1, d: 4 }).toMatchObject({ b: { c: 3 }, a: 1 });
+  });
+  test("deep equality conditional extra parameter", () => {
+    const thing = { a: 1, b: { c: 3 }, d: "important message" };
+    expect(thing).toMatchObject({
+      a: 1,
+      b: { c: 3 },
+      ...(thing.a == 1 ? {} : { d: "display reason on fail" }),
+    });
   });
 });
